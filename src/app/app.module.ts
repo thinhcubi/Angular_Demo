1dbef2f6e0bcd643f779acd6ac99b193f2d37897
thinhcubi <thinhcubi27@gmail.com>
2021-10-25 09:43:45 +0700

    demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ImageGalleryModule} from "./image-gallery/image-gallery.module";
+import { GalleryConfig } from "./image-gallery/token";
 
 @NgModule({
   declarations: [
@@ -10,9 +12,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ImageGalleryModule
   ],
-  providers: [],
+  providers: [
+    {provide: GalleryConfig,useValue: 3}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
